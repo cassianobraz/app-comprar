46bdd00cd96e473624fbc22b24343d82f68d17ed
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2026-02-21 09:25:45 -0400

    feat: new two components filter and icon

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,15 +1,36 @@
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, Text, View } from "react-native";
 import { styles } from './styles';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+import { FilterStatus } from '@/types/FilterStatus';
+
+const FILTER_STATUS: FilterStatus[] = [
+  FilterStatus.DONE,
+  FilterStatus.PENDING,
+]
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      
+      <View style={styles.form}>
+        <Input placeholder='O que você precisa comprar' />
+        <Button title="Entrar" />
+      </View>
 
-      <Input placeholder='O que você precisa comprar' />
-      <Button title="Entrar" />
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
